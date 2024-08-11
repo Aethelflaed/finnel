d5fb7991d2fcefc8236e5cee87f8a1740cf138f7
Geoffroy Jaffa Planquart <geoffroy@planquart.fr>
2024-08-11 10:30:21 +0200

    Migration to add date indexes

---
+++ finnel/migrations/2024-08-10-073734_add_date_indexes/down.sql
@@ -0,0 +1,3 @@
+-- This file should undo anything in `up.sql`
+DROP INDEX records_operation_date;
+DROP INDEX records_value_date;
---
+++ finnel/migrations/2024-08-10-073734_add_date_indexes/up.sql
@@ -0,0 +1,3 @@
+-- Your SQL goes here
+CREATE INDEX records_value_date ON records (value_date DESC);
+CREATE INDEX records_operation_date ON records (operation_date DESC);
